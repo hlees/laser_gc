5bee64b5fa151b1e05f6d9fe85623b74e3d701f1
dhjin <dhjin@sjit.co.kr>
2014-03-07 11:05:10 +0900

    all okay??

--- common/database/models/model_base.py
+++ common/database/models/model_base.py
@@ -3,6 +3,10 @@ from sqlalchemy.ext.declarative import delcarative_base
 ModelBase = declarative_base()
 class DictionaryConvertable():
     def to_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        d = {}
+	for column in self.__table__.columns:
+	    d[column.name] = getattr(self, column.name)
+
+	return d
 
 
--- common/database/models/user.py
+++ common/database/models/user.py
@@ -4,17 +4,10 @@ from .model_base import ModelBase, DictionaryConvertable
 from .json_type import JsonType
 
 
-class User(ModelBase):
+class User(ModelBase, DictionaryConvertable):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True, nullable=False)
     device_id = Column(String(100), nullable=False)
     
     social_token = Column(String, nullable=True)
-
-    def to_dict(self):
-        d = {}
-	for column in self.__table__.columns:
-	    d[column.name] = getattr(self, column.name)
-
-	return d
