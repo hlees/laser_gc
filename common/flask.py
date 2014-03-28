064798d728ecd792e34aecf08e8c96fd93f26e2b
dhjin <dhjin@sjit.co.kr>
2014-03-28 15:37:12 +0900

    import database_url

--- common/flask.py
+++ common/flask.py
@@ -6,6 +6,8 @@ from flask import Flask, Response
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from common.common_config import database_url
+
 app = Flask(__name__)
 app.db = create_engine(database_url, pool_size=20, max_overflow=-1)
 app.db.session = scoped_session(sessionmaker(bind=engine))
