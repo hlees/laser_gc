bff595a4dacf8cce092a10332c1df8fbbb79cd92
dhjin <dhjin@sjit.co.kr>
2014-03-28 15:24:18 +0900

    add query to mobel_base

--- common/database/models/model_base.py
+++ common/database/models/model_base.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 
 ModelBase = declarative_base()
+ModelBase.query = app.db.session.query_property()
+
 class DictionaryConvertable():
     def to_dict(self):
         d = {}
